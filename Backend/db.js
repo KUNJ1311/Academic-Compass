5f5d00613ca96ec038914fe317820788a28ef81b
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-03-08 12:15:38 +0530

    changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGODB_KEY;
+
+const mongoURI = "mongodb+srv://kunjfaladu1311:Kunj131148ksf@kunj.xrpcy2r.mongodb.net/test";
 
 const connectToMongo = () => {
-	mongoose.connect(mongoURI, () => {
-		console.log("successfully connected");
-	});
+	mongoose.connect(
+		mongoURI,
+
+		() => {
+			console.log("successfully connected");
+		}
+	);
 };
 mongoose.set("strictQuery", false);
 
