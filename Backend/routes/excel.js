fe7076685cbbf75b9b44e6d470440eb41fa3b2af
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-03-08 16:55:54 +0530

    new

--- Backend/routes/excel.js
+++ Backend/routes/excel.js
@@ -20,9 +20,10 @@ const Sem8 = require("../models/Sem8");
 
 const Studentdata = require("../models/Studentdata");
 
+excel.use(express.static(path.resolve(__dirname, "excel")));
 var storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "excel");
+		cb(null, "../excel");
 	},
 	filename: (req, file, cb) => {
 		if (file.mimetype !== "text/csv") {
@@ -100,7 +101,7 @@ Semesters.forEach((semester) => {
 					}
 				}
 			}
-			fs.unlinkSync(req.file.path);
+			// fs.unlinkSync(req.file.path);
 			res.send({ status: 200, success: true, msg: "CSV Imported" });
 		} catch (error) {
 			res.send({ status: 400, success: false, msg: error.message });
@@ -164,7 +165,7 @@ Semesters.forEach((semester) => {
 					}
 				}
 			}
-			fs.unlinkSync(req.file.path);
+			// fs.unlinkSync(req.file.path);
 			res.send({ status: 200, success: true, msg: "CSV Imported" });
 		} catch (error) {
 			res.send({ status: 400, success: false, msg: error.message });
@@ -228,7 +229,7 @@ Semesters.forEach((semester) => {
 					}
 				}
 			}
-			fs.unlinkSync(req.file.path);
+			// fs.unlinkSync(req.file.path);
 			res.send({ status: 200, success: true, msg: "CSV Imported" });
 		} catch (error) {
 			res.send({ status: 400, success: false, msg: error.message });
